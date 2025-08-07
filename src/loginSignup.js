4e6bff0468ac1637c7a863fd8057fec45c4dbcdd
Gayanga Nayanajith <gayangansilva@gamail.com>
2025-08-07 09:07:58 +0530

    Modified some files

--- src/loginSignup.js
+++ src/loginSignup.js
@@ -1,29 +1,50 @@
-import React  from "react";
+import React, {useState}  from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faKeyboard, faunlock, faEnvelope } from '@fortawesome/free-regular-svg-icons';
+import { faKeyboard, faUnlock, faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import './loginSignup.css';
 
 const LoginSignup = () => {
 
+    const [showLogin, setShowLogin] = useState(false);
+
     return (
         <div id="container">
-            <div className="header">SignUp</div>
-
-            <div className="form">
-                <div className="input-name">
-                    <i className="fa-solid fa-keyboard"></i>
-                    <input type="text" placeholder="Name" />
-                </div>
-                <div className="input-email">
-                    <i className="fa-solid fa-envelope"></i>
-                    <input type="email" placeholder="Email" />
-                </div>
-                <div className="input-password">
-                    <i className="fa-solid fa-unlock"></i>
-                    <input type="password" placeholder="Password" />
-                </div>
+            <div className="header">{showLogin ? "Login" : "SignUp"}</div>
+            <div className="form-signup">
+                {showLogin ? (
+                    <>
+                         <div className="input-name">
+                            <FontAwesomeIcon icon={faKeyboard} />
+                            <input type="name" placeholder="Name" />
+                        </div>
+                        <div className="input-password">
+                            <FontAwesomeIcon icon={faUnlock} />
+                            <input type="password" placeholder="Password" />
+                        </div>
+                        <button className="btn-login">Login</button>
+                        <button className="btn-signup" onClick={() => setShowLogin(false)}>SignUp</button>
+                    </>
+                ) : (
+                    <>
+                        
+                        <div className="input-name">
+                            <FontAwesomeIcon icon={faKeyboard} />
+                            <input type="name" placeholder="Name" />
+                        </div>
+                        <div className="input-email">
+                            <FontAwesomeIcon icon={faEnvelope} />
+                            <input type="email" placeholder="Email" />
+                        </div>
+                        
+                        <div className="input-password">
+                            <FontAwesomeIcon icon={faUnlock} />
+                            <input type="password" placeholder="Password" />
+                        </div>
+                        <button className="btn-signup">SignUp</button>
+                        <button className="btn-login" onClick={() => setShowLogin(true)}>Login</button>
+                    </>
+                )}
             </div>
-
             <div className="footer"></div>
         </div>
     );
